1cef7b4b382a93ea3b7edc5d2dd4a2a260895022
Carolina Oliveira <carolina.oliveira@mbrf.com>
2026-05-06 08:34:52 -0300

    feat: implementa exceções ConflictException, DomainException e NotFoundException

---
+++ PB.Proposta/PB.Proposta.Domain/Exceptions/ConflictException.cs
@@ -0,0 +1,10 @@
+namespace PB.Proposta.Domain.Exceptions
+{
+    public class ConflictException : Exception
+    {
+        public ConflictException(string message) : base(message) { }
+    }
+}
+
+
+
---
+++ PB.Proposta/PB.Proposta.Domain/Exceptions/NotFoundException.cs
@@ -0,0 +1,7 @@
+namespace PB.Proposta.Domain.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message) { }
+    }
+}
