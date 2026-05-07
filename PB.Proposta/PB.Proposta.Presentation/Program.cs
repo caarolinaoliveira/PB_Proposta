e0d2bc2e034b7fd9879169343931ce0cfb69ed32
Carolina Oliveira <carolinaoliveira99@hotmail.com>
2026-05-06 21:54:09 -0300

    feat: atualiza target framework para net10.0

--- PB.Proposta/PB.Proposta.Presentation/Program.cs
+++ PB.Proposta/PB.Proposta.Presentation/Program.cs
@@ -39,11 +39,10 @@ builder.Services.AddHostedService<RabbitMQConsumer>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 using (var scope = app.Services.CreateScope())
 {
